e3bd5c730a40357b0d473c69ccd472e29e04c073
jmkim <jmkim@singleton.co.kr>
2020-07-16 16:35:59 +0900

    JS: Navbar menu-click

--- main.js
+++ main.js
@@ -1,12 +1,28 @@
 'use strict';
 
 // https://developer.mozilla.org/en-US/docs/Web/API/CSS_Object_Model/Determining_the_dimensions_of_elements
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
 
 // Make navbar transparent when it is on the top
 document.addEventListener('scroll', () => {
+  // console.log(window.scrollY);
+  // console.log(`navbarHeight: ${navbarHeight}`);
   if (window.scrollY > navbarHeight) {
     navbar.classList.add('navbar--dark');
   } else {
     navbar.classList.remove('navbar--dark');
   }
+});
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+  // console.log(event.target.dataset.link);
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) return;
+  const scrollTo = document.querySelector(link);
+  console.log(scrollTo);
+  scrollTo.scrollIntoView({ behavior: 'smooth' });
 });
